391ee0ef24e41fb9f9a4e5eb2a1e714a1ff240bc
@fatestatenight <1921026512@qq.com>
2023-06-12 17:22:03 +0800

    修改联系人搜索1

---
+++ app/admin/controller/integral/Contact.php
@@ -0,0 +1,54 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | App Integral Exchange
+// +----------------------------------------------------------------------
+// | 独立页面
+// +----------------------------------------------------------------------
+// |
+// +----------------------------------------------------------------------
+// | 不限制登录状态
+// +----------------------------------------------------------------------
+
+namespace app\admin\controller\integral;
+
+
+use app\admin\model\IntegralContact;
+use app\common\controller\AdminController;
+//use EasyAdmin\annotation\ControllerAnnotation;
+//use EasyAdmin\annotation\NodeAnotation;
+use think\App;
+use think\facade\Log;
+
+/**
+ * Class Draw
+ * @package app\admin\controller\integral
+ */
+class Draw extends AdminController
+{
+
+    //use \app\admin\traits\Curd;
+
+    protected $sort = [
+        'sort' => 'desc',
+        'id'   => 'desc',
+    ];
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+        $this->model = new IntegralContact();
+    }
+
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function searchContact()
+    {
+        $data = file_get_contents('php://input');
+        var_dump($data);die;
+    }
+
+}
